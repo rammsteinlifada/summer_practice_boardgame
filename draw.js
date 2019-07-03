60b2ae8e9de311c8f05084f9cc38cde2f9e6601c
Svetlana <vashchenko.siu@students.dvfu.ru>
2019-07-03 21:04:20 +1000

    Design 1

--- draw.js
+++ draw.js
@@ -2,4 +2,14 @@ window.addEventListener("DOMContentLoaded", function()
 {
 	var cvs = document.getElementById("canvas");
 	var ctx = cvs.getContext("2d");
+	
+	
+	var img = new Image();   // Создаём новый объект Image
+	
+	img.onload = function() {
+		ctx.drawImage(img, 140, 60, 30, 40);
+	};
+
+// Загружаем файл изображения
+img.src = "output/y_70368999ca_1.jpg";
 });
