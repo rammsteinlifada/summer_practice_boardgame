b79b350bcafcbd01102675ea91c072cf488f4ad5
Svetlana <vashchenko.siu@students.dvfu.ru>
2019-07-05 15:44:08 +1000

    draw v2

--- draw.js
+++ draw.js
@@ -4,32 +4,34 @@ window.addEventListener("DOMContentLoaded", function()
 	var ctx = cvs.getContext("2d");
 	
 	
-	var img = new Image();   // Создаём новый объект Image
+	var img = new Image();
 	
 	img.onload = function() {
 		ctx.drawImage(img, 140, 60, 30, 40);
 	};
 
 // Загружаем файл изображения
-img.src = "output/y_70368999ca_1.jpg";
+img.src = "background.jpg";
 });
 
 
 
 class Deck {
-	constructor {
+	deck = new Array();
+	create() {
 		for (var i = 0; i < 48; i++) {
 			
 		}
 	}
-	var deck = [];
+	
 	
 }
 
 //0 - field; 1 - stadium; 2 - road, 3 - building
 class card {
-	contains[5];
-	constructor(name) {
+	contains = new Array();
+	
+	ident(name) {
 		for (var i = 0; i < 5; i++) {
 			switch (name[i]) {
 				case 'f':
@@ -48,6 +50,6 @@ class card {
 		}
 	}
 	remote(name) {
-
+		name = name[3] + name.substring(0, 3) + name.substring(4, 6);
 	}
 }
