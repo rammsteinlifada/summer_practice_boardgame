f79fea7bf06b7e97e3d144a12279843477f1f6e8
Svetlana <vashchenko.siu@students.dvfu.ru>
2019-07-08 17:57:10 +1000

    solving conflicts

--- classes.js
+++ classes.js
@@ -252,24 +252,23 @@ class Deck {
 				var newY = e.pageY - e.pageY % 100 - 200;
 				var i = newX / 100;
 				var j = newY / 100;
-				console.log(a);
-				console.log(r.f.field[i][j + 1]);
-				console.log(i,j);
+
 				if ((e.screenX) < 1000 && (e.screenY.toFixed()) > 200 && (e.screenY.toFixed()) < 1200) {
 					if (!r.f.field[i][j]) {
 						if (((!r.f.field[i - 1][j]) || (r.f.field[i - 1][j][2] == a[0])) && ((!r.f.field[i + 1][j]) || (r.f.field[i + 1][j][0] == a[2])) &&
 							((!r.f.field[i][j - 1]) || (r.f.field[i][j - 1][3] == a[1])) && ((!r.f.field[i][j + 1]) || (r.f.field[i][j + 1][1] == a[3]))) {
+
 							r.ctx.drawImage(image, newX, newY);
 							r.f.field[i][j] = a;
 							document.onmousemove = null;
 							im.onmouseup = null;
 							im.remove();
 						}
-						else{
+
+						else {
 							document.onmousemove = null;
 							im.onmouseup = null;
-						}
-
+						};
 					};
 				};
 
